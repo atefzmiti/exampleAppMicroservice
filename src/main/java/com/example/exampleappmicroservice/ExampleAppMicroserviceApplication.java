5e4af9986103b7ffe02684455ee57af42db9c6ba
atefzm <zmitiatef@gmail.com>
2025-07-08 11:16:56 +0100

    fix build

--- src/main/java/com/example/exampleappmicroservice/ExampleAppMicroserviceApplication.java
+++ src/main/java/com/example/exampleappmicroservice/ExampleAppMicroserviceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -16,7 +15,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @AutoConfiguration(after = { DataSourceAutoConfiguration.class })
 @EnableConfigurationProperties(JpaProperties.class)
 @EnableFeignClients
-@EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.example.exampleappmicroservice.DAO")
 @EnableElasticsearchRepositories(basePackages = "com.example.exampleappmicroservice.DAO.elasticsearch")
 public class ExampleAppMicroserviceApplication {
